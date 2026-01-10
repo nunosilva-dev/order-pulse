1abaa648e4ab285da75cb0830d43e9e75b19af11
Nuno Silva <nunosilva.code@gmail.com>
2026-01-10 11:55:30 +0000

    init project structure with healthy db and kafka connection

---
+++ src/main/java/com/nsdev/orderpulse/OrderPulseApplication.java
@@ -0,0 +1,12 @@
+package com.nsdev.orderpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPulseApplication.class, args);
+    }
+}
